444f9c5890e5cae5f487c5d6ab8f0b19105974bb
HoJong <pjh00098@gmail.com>
2022-04-27 15:43:18 +0900

    Chore: Add Xcode Project

---
+++ Gesture-Study/Gesture-Study/Gesture_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gesture_StudyApp.swift
+//  Gesture-Study
+//
+//  Created by JongHo Park on 2022/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Gesture_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
