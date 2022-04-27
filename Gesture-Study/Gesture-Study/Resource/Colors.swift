6d73ab2864678e840c17e95ffc3a35af2ff29237
HoJong <pjh00098@gmail.com>
2022-04-27 17:28:18 +0900

    Chore: Add color assets
    Chore: Add Color Assets
    
    
    Chore: Add color assets

---
+++ Gesture-Study/Gesture-Study/Resource/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  Gesture-Study
+//
+//  Created by JongHo Park on 2022/04/27.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  static let primary: Color = Color("PrimaryColor")
+  static let grayButton: Color = Color("GrayColor")
+  static let subText: Color = Color("SubTextColor")
+  static let stuTop: Color = Color("SwipeToUnlockTopColor")
+  static let stuBottom: Color = Color("SwipeToUnlockBottomColor")
+  static let stuBtnTop: Color = Color("SwipeToUnlockButtonTopColor")
+  static let stuBtnCenter: Color = Color("SwipeToUnlockButtonCenterColor")
+  static let stuBtnBottom: Color = Color("SwipeToUnlockButtonBottomColor")
+  static let background: Color = Color("BackgroundColor")
+}
