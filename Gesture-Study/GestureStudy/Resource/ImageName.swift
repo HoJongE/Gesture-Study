b4dcbc28a181d4a8275bcc094b7c673642f5db26
HoJong <pjh00098@gmail.com>
2022-04-27 21:02:54 +0900

    Docs: Add gesture-study project image assets
    Resolves: #2
    Docs: Add Gesture-Study Project Image Assests
    
    Resolves: #2

---
+++ Gesture-Study/GestureStudy/Resource/ImageName.swift
@@ -0,0 +1,17 @@
+//
+//  ImageName.swift
+//  GestureStudy
+//
+//  Created by JongHo Park on 2022/04/27.
+//
+
+import Foundation
+
+enum ImageName {
+  static let doubleTap: String = "DoubleTap"
+  static let dragAndDrop: String = "DragAndDrop"
+  static let flick: String = "Flick"
+  static let swipe: String = "Swipe"
+  static let swipeToUnlock: String = "SwipeToUnlock"
+  static let longPress: String = "LongPress"
+}
