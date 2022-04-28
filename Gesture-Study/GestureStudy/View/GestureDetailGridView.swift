364053624befaa6d8cdff561ae9ac09d08cafcdf
HoJong <pjh00098@gmail.com>
2022-04-28 16:03:32 +0900

    Feat: Add gesture detail grid item

---
+++ Gesture-Study/GestureStudy/View/GestureDetailGridView.swift
@@ -0,0 +1,50 @@
+//
+//  GestureDetailGridView.swift
+//  GestureStudy
+//
+//  Created by JongHo Park on 2022/04/28.
+//
+
+import SwiftUI
+
+// MARK: 제스처 디테일 그리드뷰
+struct GestureDetailGridView: View {
+    var body: some View {
+      GestureDetailGridItem(gestureDetail: Tap())
+    }
+}
+// MARK: 제스처 디테일 그리드 아이템
+struct GestureDetailGridItem<Detail: GestureDetailProtocol>: View {
+
+  let gestureDetail: Detail
+
+  var body: some View {
+    VStack(alignment: .center, spacing: 2) {
+      gestureDetail.image
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .frame(width: 52, height: 52)
+        .foregroundColor(.brand)
+        .padding(.init(top: 34, leading: 0, bottom: 25, trailing: 0))
+
+      Text(gestureDetail.enNm)
+        .font(.title3)
+        .bold()
+
+      Text(gestureDetail.korNm)
+        .font(.caption)
+        .foregroundColor(.subText)
+        .padding(.bottom, 10)
+    }
+    .lineLimit(1)
+    .frame(maxWidth: 300)
+    .background(RoundedRectangle(cornerRadius: 8).fill(Color.gridItemBackground).shadow(radius: 2))
+    .padding(.horizontal, 8)
+  }
+}
+// MARK: 제스처 디테일 프리뷰
+struct GestureDetailGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GestureDetailGridView()
+    }
+}
