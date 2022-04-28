809f75f45cfef0a19515da41b8bc0b002634e1d2
HoJong <pjh00098@gmail.com>
2022-04-28 11:14:16 +0900

    Style: Fix swiftlint issue
    Style: Fix code with control+i

--- Gesture-Study/GestureStudy/Gesture_StudyApp.swift
+++ Gesture-Study/GestureStudy/Gesture_StudyApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct GestureStudyApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
